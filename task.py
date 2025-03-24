83f8f96f887c0fe8e520638ea9927cf8aee312d1
abdulbasit911786 <zubaidzubaidullah7@gmail.com>
2025-03-25 01:47:30 +0500

    Band Name Generator

---
+++ task.py
@@ -0,0 +1,14 @@
+print("Your band name generator")
+city = input("What is your city name?\n")
+pet = input("What is your pet name?\n")
+print("Your band name could be: " + city + " " + pet)
+
+
+city = input("What is the name of city you visit in weekend?\n")
+place = input("In city which place you visit in weekend?\n")
+Reason = input("Reason for visit?\n")
+me = input("Why you not tell me?\n")
+
+
+print("This could be: " + city + " " + place + " " + Reason + "," + " " + me)
+print("😂😂😂 ➡️ yuchen")
